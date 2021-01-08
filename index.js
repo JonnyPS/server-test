0ad4c0e170b9517eace16295c3298d7629b86069
Jonny Spry <j.spry@arts.ac.uk>
2021-01-08 10:29:26 +0000

    add html page

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 5000;
+const path = require("path");
 
-app.get('/', (req, res) => {
+app.get('/my-route', (req, res) => {
 	res.send('hello world')
+})
+
+// all requests that aren't caught by our other api endpoints should direct to the index html file
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 app.listen(port, () => console.log(`Your app is now running at ${port}`))
