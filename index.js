6f46aee183e5d767ec95984c4c0106131a49771e
Jonny Spry <j.spry@arts.ac.uk>
2021-01-08 15:42:05 +0000

    connect db

--- index.js
+++ index.js
@@ -2,13 +2,67 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 5000;
 const path = require("path");
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('abcd');
 
-app.get('/my-route', (req, res) => {
-	res.send('hello world')
+db.serialize(function() {
+	let create = `
+		CREATE TABLE IF NOT EXISTS Persons (
+			id INTEGER PRIMARY KEY,
+			LastName varchar(255) NOT NULL,
+			FirstName varchar(255),
+			Age int
+		);
+	`;
+
+	let insert = `
+		INSERT INTO Persons (LastName, FirstName, Age) VALUES (
+			'someone',
+			'jon',
+			30
+		);
+	`;
+
+	db.run(create, (error) => {
+		error ? console.log( 'TABLE CREATION Error ------', error ) : console.log('Table successfully created');
+
+		db.run(insert, (error) => {
+			error ? console.log( 'Error ------', error ) : console.log('Data successfully inserted into table');
+			// db.close();
+			// let sqlQuery = `SELECT * FROM Persons`;
+			// db.each(sqlQuery, [], (err, row) => {
+			// 	if (err) {
+			// 		throw err;
+			// 	}
+			// 	console.log(row.id + ' ' + row.FirstName);
+			// });
+		});
+		})
+});
+
+
+	
+	
+	
+  // db.each("SELECT id, dt FROM user", function(err, row) {
+  //     console.log("User id : "+row.id, row.dt);
+  // });
+
+
+app.get('/', (req, res) => {
+	let sqlQuery = `SELECT * FROM Persons`;
+	db.all(sqlQuery, [], (err, rows) => {
+		if (err) {
+			throw err;
+		}
+		res.json({
+			"data": rows
+		})
+	});
 })
 
 // all requests that aren't caught by our other api endpoints should direct to the index html file
-app.get('/', (req, res) => {
+app.get('/my-route', (req, res) => {
 	res.sendFile(path.join(__dirname + '/index.html'));
 })
 
